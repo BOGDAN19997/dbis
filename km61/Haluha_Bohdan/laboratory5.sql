9a1459a0c24de26b9cf4746027973b0f50f4b631
BOGDAN19997 <38032410+BOGDAN19997@users.noreply.github.com>
2018-06-04 16:08:22 +0300

    Update laboratory5.sql

--- km61/Haluha_Bohdan/laboratory5.sql
+++ km61/Haluha_Bohdan/laboratory5.sql
@@ -1,2 +1,22 @@
 -- LABORATORY WORK 5
 -- BY Haluha_Bohdan
+Завдання 1
+
+DELIMITER //
+
+DROP PROCEDURE count_orders
+
+CREATE PROCEDURE count_orders (IN i INT, OUT ord_count INT)
+  begin
+    DECLARE item_count INT
+    DROP VIEW IF EXISTS report_orders;
+    DECLARE VIEW report_orders AS SELECT orderitems.order_item, orders.cust_id
+      orderitems.quantity, orderitems.order_item*orderitems.quantity AS summa
+    FROM orderitems, orders
+    WHERE orderitems.order_num = orders.order_num;
+    SELECT SUM(summa) AS item_count FROM report_orders WHERE cust_id=i
+  end
+  //
+  
+  
+  SELECT SUM(summa) INTO prod_num FROM report_orders WHERE cust_id=i
