975009052f172999bbf2ac87af3e1ca36bc17455
BOGDAN19997 <38032410+BOGDAN19997@users.noreply.github.com>
2018-06-09 10:04:13 +0300

    Update laboratory4.sql

--- km61/Haluha_Bohdan/laboratory4.sql
+++ km61/Haluha_Bohdan/laboratory4.sql
@@ -1,2 +1,20 @@
 -- LABORATORY WORK 4
 -- BY Haluha_Bohdan
+/*1.Написати тріггер, що при видаленні повідомлень, змінює текст повідомлень на "видалено"*/
+CREATE TRIGGER trigg_1
+  BEGIN
+    UPDATE messeges
+    IF SQL% NOT FOUND THEN
+      dbms_output.put_line('видалено')
+  END;
+  
+  
+  
+/*2.При зміні ім'я вендора змінюється його vend_city на 'Київ'*/
+
+DECLARE var_rows number(5)
+BEGIN
+  UPDATE  vendors
+  IF vend_name.change
+    SET vendors.vend_city = 'Київ'  
+  
